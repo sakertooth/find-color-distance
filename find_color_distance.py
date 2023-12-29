432948e89e9d80119548c7c0bde1e5b49125aa19
saker <sakertooth@gmail.com>
2023-12-29 02:27:21 -0500

    Fix sorting and remove duplicates

--- find_color_distance.py
+++ find_color_distance.py
@@ -5,6 +5,7 @@ import re
 import sys
 import math
 
+
 def color_from_input(color_input):
     tuple_match = re.match(r"\( *(\d*) *, *(\d*) *, *(\d*) *\)", color_input)
     hex_match = re.match(
@@ -18,13 +19,15 @@ def color_from_input(color_input):
         red, green, blue = [int(val, 16) for val in hex_match.groups()]
     else:
         raise RuntimeError(f"Invalid color input: {color_input}")
-    return {"input": color_input.strip(), "red": int(red), "green": int(green), "blue": int(blue)}
+    return (color_input.lower().strip(), int(red), int(green), int(blue))
+
 
 def find_distance(target, color):
-    target_point = (target['red'], target['green'], target['blue'])
-    color_point = (color['red'], color['green'], color['blue'])
+    target_point = target[1:]
+    color_point = color[1:]
     color_distance_from_target = math.dist(color_point, target_point)
-    return (target['input'], color['input'], f'{round(color_distance_from_target, 2)}')
+    return (target[0], color[0], round(color_distance_from_target, 2))
+
 
 parser = argparse.ArgumentParser(
     prog="Color Distance Calculator",
@@ -34,7 +37,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "targets",
     help="The target colors (either list of (red, green, blue) or hex values)",
-    nargs="+"
+    nargs="+",
 )
 
 parser.add_argument(
@@ -51,10 +54,14 @@ if args.palette == None and args.infile == None:
 targets = [color_from_input(color) for color in args.targets]
 
 palette = []
-if '-' in args.palette:
+if "-" in args.palette:
     palette.extend([color_from_input(line) for line in sys.stdin])
 else:
-    palette.extend([color_from_input(color) for color in args.palette if color != '-'])
+    palette.extend([color_from_input(color) for color in args.palette if color != "-"])
+
+targets = set(targets)
+palette = set(palette)
 
 results = [find_distance(target, color) for color in palette for target in targets]
-print(tabulate(results, headers=['Target', 'Color', 'Distance']))
+results = sorted(results, key=lambda result: result[2])
+print(tabulate(results, headers=['Target', 'Color', 'Distance'], stralign='left'))
